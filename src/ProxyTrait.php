b9751188236399d218252f71dc5ec69b2944edd8
Alexander Makarov <sam@rmcreative.ru>
2021-08-02 16:49:28 +0300

    Revert "Fix `ProxyTrait::repeatError()` (#15)"
    This reverts commit 4cb71d49e633509a2613b38bd701cf516b829ba1.

--- src/ProxyTrait.php
+++ src/ProxyTrait.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace Yiisoft\Proxy;
 
-use Throwable;
-
 trait ProxyTrait
 {
     private ?object $currentError = null;
@@ -15,13 +13,11 @@ trait ProxyTrait
         return $this->currentError === null ? 'success' : 'failed';
     }
 
-    /**
-     * @throws Throwable
-     */
-    protected function repeatError(Throwable $error): void
+    protected function repeatError(\Throwable $error): void
     {
-        $this->currentError = clone $error;
-        throw $error;
+        $this->currentError = $error;
+        $errorClass = get_class($error);
+        throw new $errorClass($error->getMessage());
     }
 
     protected function resetCurrentError(): void
