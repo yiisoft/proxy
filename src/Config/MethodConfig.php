aa967d8527c39d5b94b52c07f4964714f0758000
Alexey Rogachev <arogachev90@gmail.com>
2022-06-08 17:28:42 +0600

    Add docs for MethodConfig

--- src/Config/MethodConfig.php
+++ src/Config/MethodConfig.php
@@ -4,22 +4,37 @@ declare(strict_types=1);
 
 namespace Yiisoft\Proxy\Config;
 
+/**
+ * A method metadata. {@see ClassConfigFactory} is used for creation.
+ */
 final class MethodConfig
 {
     public function __construct(
         /**
-         * @var string[]
+         * @var string[] A list of modifiers, for example: `['abstract', 'public']`.
          */
         public array $modifiers,
+        /**
+         * @var string A name without parentheses. For example: `getInstance`.
+         */
         public string $name,
         /**
-         * @var ParameterConfig[]
+         * @var ParameterConfig[] A map where key is a {@see ParameterConfig::$name} and value is {@see ParameterConfig}
+         * instance.
+         * @psalm-var array<string, ParameterConfig>
          */
         public array $parameters,
+        /**
+         * @var TypeConfig|null Return type config. `null` means no return type specified.
+         */
         public ?TypeConfig $returnType,
     ) {
     }
 
+    /**
+     * Whether a method has return type.
+     * @return bool `true` if return type specified and `false` otherwise.
+     */
     public function hasReturnType(): bool
     {
         return $this->returnType !== null;
